c55d4448d680dd8f76a09b9647704fda4d4e58ed
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-02 00:29:28 +0300

    final grad_data  added

---
+++ studens_grade/grad_data.py
@@ -0,0 +1,14 @@
+def get_students():
+    students = [
+    ("Dana", 82),
+    ("Tom", 55),
+    ("Maya", 91),
+    ("Ron", 60),
+    ("Noa", "88"),
+    ("Ben", 120),
+    ("Lior", -5),
+    (55, 70),
+    ["bob",5 ] 
+] 
+    return students 
+
