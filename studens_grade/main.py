1894eb654ec7b46ee1d00d6c7eb666e4decec32c
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-02 00:27:39 +0300

    final main added

--- studens_grade/main.py
+++ studens_grade/main.py
@@ -1,36 +1,10 @@
-from grad_data import get_students
 from grade_validation import gen_validate
+from grade_logic import grade_cheking
+from grade_output import statistics
+
 def main():
-    students=get_students()
-    validation=gen_validate()
-    passed_students=0
-    avrage=0
-    sum=0
-    for student in students:
-        if len(student)==2:
-            if type(student[1])==int:
-                if 0< student[1] <100:
-                    if type(student[0])==str:
-                        if type(student)==tuple:
-                            if 100> student[1]>=90:
-                                print(student[0], student[1], "Excellent")
-                            elif 60<= student[1] <=89:
-                                print(student[0], student[1], "passed")
-                            elif 0< student[1] <60:
-                                print(student[0],student[1], "failed")
-                            passed_students+=1
-                            sum+=student[1]      
-                        else:
-                            print("Data must be a tuple")        
-                    else:
-                        print("Name must be a string")
-                else:
-                    print("Grade must be between 0 and 100")
-            else:
-                print("Grade must be an integer")
-        else:
-            print("the tuple contains more then 2")
-    avrage=sum/passed_students        
-    print("passed students:", passed_students)
-    print("avrage:", avrage)
+    students = gen_validate()
+    grade_cheking()
+    statistics(students)
+
 main()
