42bcf72cf78e09c03a03527c177a2a2c61ecf94d
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-01 16:50:13 +0300

    main changes added

--- studens_grade/main.py
+++ studens_grade/main.py
@@ -1,41 +1,36 @@
-students = [
-    ("Dana", 82),
-    ("Tom", 55),
-    ("Maya", 91),
-    ("Ron", 60),
-    ("Noa", "88"),
-    ("Ben", 120),
-    ("Lior", -5),
-    (55, 70),
-    ["bob",5 ] 
-]
-passed_students=0
-sum=0
-avrage=0
-for student in students:
-    if len(student)==2:
-        if type(student[1])==int:
-            if 0< student[1] <100:
-                if type(student[0])==str:
-                    if type(student)==tuple:
-                        if 100> student[1]>=90:
-                            print(student[0], student[1], "Excellent")
-                        elif 60<= student[1] <=89:
-                            print(student[0], student[1], "passed")
-                        elif 0< student[1] <60:
-                            print(student[0],student[1], "failed")
-                        passed_students+=1
-                        sum+=student[1]      
+from grad_data import get_students
+from grade_validation import gen_validate
+def main():
+    students=get_students()
+    validation=gen_validate()
+    passed_students=0
+    avrage=0
+    sum=0
+    for student in students:
+        if len(student)==2:
+            if type(student[1])==int:
+                if 0< student[1] <100:
+                    if type(student[0])==str:
+                        if type(student)==tuple:
+                            if 100> student[1]>=90:
+                                print(student[0], student[1], "Excellent")
+                            elif 60<= student[1] <=89:
+                                print(student[0], student[1], "passed")
+                            elif 0< student[1] <60:
+                                print(student[0],student[1], "failed")
+                            passed_students+=1
+                            sum+=student[1]      
+                        else:
+                            print("Data must be a tuple")        
                     else:
-                        print("Data must be a tuple")        
+                        print("Name must be a string")
                 else:
-                    print("Name must be a string")
+                    print("Grade must be between 0 and 100")
             else:
-                print("Grade must be between 0 and 100")
+                print("Grade must be an integer")
         else:
-            print("Grade must be an integer")
-    else:
-        print("the tuple contains more then 2")
-avrage=sum/passed_students        
-print("passed students:", passed_students)
-print("avrage:", avrage)
+            print("the tuple contains more then 2")
+    avrage=sum/passed_students        
+    print("passed students:", passed_students)
+    print("avrage:", avrage)
+main()
