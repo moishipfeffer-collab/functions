57399c9e7cf475efa6afa4bc8775d79f038fab6e
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-05 00:43:37 +0300

    output added

---
+++ number_analyzer/number_output.py
@@ -0,0 +1,7 @@
+def print_number_result(number, sign, even_odd):
+    print(number, "-", sign, "-", even_odd)
+def print_skipped_number(error):
+    print("skipped number: ",error)
+def print_number_summary(average, above_average_count):
+    print("average: ",average)
+    print("above average count: ",above_average_count)
