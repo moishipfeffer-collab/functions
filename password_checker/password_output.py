b273128904f9f319ce54d245c60de7e44915eb24
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-07-04 23:20:26 +0300

    output added

---
+++ password_checker/password_output.py
@@ -0,0 +1,8 @@
+def print_password_result(password,strength):
+    print(password, "-", strength)
+
+def print_skipped_password(error):
+    print("skiped password:", error)
+
+def print_strong_passwords_count(count):
+    print("Strong passwords:", count)
